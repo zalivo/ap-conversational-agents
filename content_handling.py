34d0e46a86be0e86c7c8d4bd0cb74c61ed18308c
Ho Hoang Phuoc <69688044+hhoangphuoc@users.noreply.github.com>
2024-05-26 18:02:26 +0200

    added set up file for content_handling and knowledge graph

---
+++ content_handling.py
@@ -0,0 +1,2 @@
+# TODO: Adding the functions to handling different edge cases of the conversation
+# including: error in user content, topics changes, previous topic, etc.
---
+++ knowledge_connection.py
@@ -0,0 +1,3 @@
+# TODO: This file is used to connect the existing knowledge graph to the GPT-4 system prompt,
+# and using it to determined correct and valid responses, retrieving from the 
+# provided database. 
